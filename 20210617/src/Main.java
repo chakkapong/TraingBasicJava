2cf65230d110edc2eb54e472e91ea46d415fab9d
chakkapong.ch@kbtg.tech <chakkapong.ch@kbtg.tech>
2021-06-17 12:08:57 +0700

    constructor

---
+++ 20210617/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee test1 = new Employee();
+        String  result = test1.getFullName();
+        System.out.println(result);
+    }
+}
