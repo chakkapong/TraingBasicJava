22bd3afbdb4cfe821b48339257358ccde25a8834
chakkapong.ch@kbtg.tech <chakkapong.ch@kbtg.tech>
2021-06-17 11:30:15 +0700

    first commit

---
+++ 20210621/src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    private int id;
+    private  String fname;
+    private  String lname;
+    private  String pos;
+    void doSmt(){
+
+    }
+    public String getFullName() {
+        return fname + " : " + lname;
+    }
+
+    String getPositionName(){
+        return  "";
+    }
+}
