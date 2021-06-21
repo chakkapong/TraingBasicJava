a0a4205e4138f7613f4455bbb493ea120dc26253
chakkapong.ch@kbtg.tech <chakkapong.ch@kbtg.tech>
2021-06-21 11:54:00 +0700

    Get employee with validate number

--- src/main/java/com/example/Hellorest/employee/EmployeeController.java
+++ src/main/java/com/example/Hellorest/employee/EmployeeController.java
@@ -1,13 +1,22 @@
 package com.example.Hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
     @GetMapping("/employee/{id}")//ประกาศ Get
-    public EmployeeResponse getEmployeeByID(int id){
-        return new EmployeeResponse(id,"Chakkapong","Chaowannasiri");
+    public EmployeeResponse getEmployeeByID(@PathVariable String id){
+        //validate id => Number Only
+        int _id = 0;
+       try {
+           _id = Integer.parseInt(id);
+       }
+       catch(Exception e){
+
+       }
+        return new EmployeeResponse(_id,"Chakkapong","Chaowannasiri");
     }
 }
